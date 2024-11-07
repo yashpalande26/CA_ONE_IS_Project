c466820ce903e0ef878d369d0e3c50a3109feebf
Yash Palande <20043004@mydbs.ie>
2024-11-07 10:47:28 +0000

    Add route to Home page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Footer, Navbar } from './components/layout';
 import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 
 function App(){
   return(
-    <>
+    <BrowserRouter>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
       <Footer/>
-    </>
+    </BrowserRouter>
   )
 }
 
